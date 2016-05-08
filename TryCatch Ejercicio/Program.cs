8b299793c1f03ab87c72e6d0e187cf2782cab6b5
iscgonzpame@gmail.com <tamaulipasb1>
2016-05-08 05:53:11 -0700

    Edades
    Capturando edades

---
+++ TryCatch Ejercicio/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad=0;
+            Console.WriteLine("Introduzca su edad:");
+            
+                edad = CapturaEdad(edad);
+            
+            
+
+            Console.WriteLine("Su edad es:{0}", edad);
+            Console.ReadKey();
+        }
+
+        private static int CapturaEdad(int edad)
+        {
+            try
+            {
+                edad = Int16.Parse(Console.ReadLine());
+                return edad;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("ERROR:Eres una antiguedad.", Int16.MaxValue);
+                return CapturaEdad(edad);
+            }
+
+
+            catch (FormatException)
+            {
+                Console.WriteLine("El formato no es valido.");
+                return CapturaEdad(edad);
+            }
+        }
+    }
+}
